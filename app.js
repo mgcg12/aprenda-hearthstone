8ff960f4b313bcbb683db3181c2ef4ced3b55a43
Matheus <mgcg12@gmail.com>
2017-09-21 13:34:16 -0300

    Now we have a text editor

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var mongoose = require("mongoose");
 var path = require('path');
 var ejs = require('ejs-html');
 var session = require('express-session');
-var bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -43,40 +42,40 @@ var UserSchema = new mongoose.Schema({
 });
 
 
-// autenticar usuário
+// // autenticar usuário
 
-UserSchema.statics.authenticate = function (username, password, callback) {
-  User.findOne({ username: username })
-    .exec(function (err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-}
-//
+// UserSchema.statics.authenticate = function (username, password, callback) {
+//   User.findOne({ username: username })
+//     .exec(function (err, user) {
+//       if (err) {
+//         return callback(err)
+//       } else if (!user) {
+//         var err = new Error('User not found.');
+//         err.status = 401;
+//         return callback(err);
+//       }
+//       bcrypt.compare(password, user.password, function (err, result) {
+//         if (result === true) {
+//           return callback(null, user);
+//         } else {
+//           return callback();
+//         }
+//       })
+//     });
+// }
+// //
 
-//hashing a password before saving it to the database
-UserSchema.pre('save', function (next) {
-  var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash){
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
-});
+// //hashing a password before saving it to the database
+// UserSchema.pre('save', function (next) {
+//   var user = this;
+//   bcrypt.hash(user.password, 10, function (err, hash){
+//     if (err) {
+//       return next(err);
+//     }
+//     user.password = hash;
+//     next();
+//   })
+// });
 
 var User = mongoose.model('User', UserSchema);
 
